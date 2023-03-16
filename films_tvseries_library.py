879149f13896c67b785ef5c91a4e6ad916a13788
Szymon Misztal <sz.misztal@gmail.com>
2023-03-16 11:55:35 +0100

    add films and tvseries classes

---
+++ films_tvseries_library.py
@@ -0,0 +1,18 @@
+class Films():
+    def __init__(self, title, publication_date, genre, number_of_plays):
+        self.title = title
+        self.publication_date = publication_date
+        self.genre = genre
+        self.number_of_plays = number_of_plays
+
+    def __str__(self):
+        return f"{self.title}, {self.publication_date}, {self.genre}, {self.number_of_plays}"
+
+class TVSeries(Films):
+    def __init__(self, episode_number, season_number, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.episode_number = episode_number
+        self.season_number = season_number
+    
+    def __str__(self):
+        return super(). __str__ + f"{self.episode_number}, {self.season_number}"
